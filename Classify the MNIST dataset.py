e701607ebd64162c77362614b13a5875e39afb62
Kevinisagirl <kevin.ann.hunt@gmail.com>
2018-02-14 18:27:47 -0800

    adding a try and except for error handling. added regions to collapse code

--- Classify the MNIST dataset.py
+++ Classify the MNIST dataset.py
@@ -6,25 +6,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import tempfile
+import sys
 
-# add in error handling for when there is a timeout
+#region Grabbing and Splitting the Data
 
-###########################################
-####  Grabbing and Splitting the Data  ####
-###########################################
-# grab the MNIST data
-test_data_home = tempfile.mkdtemp()
-mnist = fetch_mldata('MNIST original', data_home=test_data_home)
+# grab the MNIST data and store to a temporary location
+# catch the error for frequent HTTP Error 500's
+try:
+    test_data_home = tempfile.mkdtemp()
+    mnist = fetch_mldata('MNIST original', data_home=test_data_home)
+except Exception as ex:
+    print(str(ex) + "\nI don't know... this happens sometimes. Run Again.")
+    sys.exit(1)
 
 # Print to show there are 70000 images (28 by 28 images for a dimensionality of 784)
-print("Image Data Shape" , mnist.data.shape)
+print("Image Data Shape", mnist.data.shape)
 
 # split the data into a training and test set
-train_img, test_img, train_lbl, test_lbl = train_test_split(mnist.data,
-                                                            mnist.target,
-                                                            test_size=1/7.0,
-                                                            random_state=0)
+train_img, test_img, train_lbl, test_lbl = train_test_split(mnist.data, mnist.target,
+                                                            test_size=1/7.0, random_state=0)
 
+"""
 # for sanity, check the data and labels have been properly loaded and split
 print(train_img.shape)
 print(train_lbl.shape)
@@ -38,59 +40,51 @@ for index, (image, label) in enumerate(zip(train_img[0:5], train_lbl[0:5])):
     plt.imshow(np.reshape(image, (28, 28)), cmap=plt.cm.gray)
     plt.title('Training: %i\n' % label, fontsize = 20)
 plt.show()
+"""
+#endregion
 
-###########################################
-#####  Model Training and Prediction  #####
-###########################################
+#region Model Training and Prediction
 
 # make an instance of the model
 # we will use L-BFGS optimization
-logisticRegr = LogisticRegression(solver = 'lbfgs')
+logisticRegr = LogisticRegression(solver='lbfgs')
 
 # train the model on the data (model learns the relationship btwn the image and true number)
 logisticRegr.fit(train_img, train_lbl)
 
 # using this trained model, predict the labels of the test set
 # returns a numpy array
-# predict for 1 obs
-singlePrediction = logisticRegr.predict(test_img[0].reshape(1, -1))
-print(singlePrediction)
-
-# predict multiple
-multPredictions = logisticRegr.predict(test_img[0:10])
-print(multPredictions)
 
 # predict the entire test set
 fullPrediction = logisticRegr.predict(test_img)
 
-###########################################
-######  Measuring Model Performance  ######
-###########################################
+#endregion
+
+#region Measuring Model Performance
 
 # accuracy as fraction of correct predictions
 # calculated by correct predictions/total number of predictions
 # this can be done using the score method
 score = logisticRegr.score(test_img, test_lbl)
-print(score)
 
 # we can also use a confusion matrix to visualize the performance of a classification model
 cm = metrics.confusion_matrix(test_lbl, fullPrediction)
-# use this line to normalize the confusion matrix to show percentages instead of counts
+# normalize the confusion matrix to show percentages instead of counts
 cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
 
 # plot this matrix
 plt.figure(figsize=(8, 8))
 sns.heatmap(cm, annot=True, fmt=".3f", linewidths=.5, square = True, cmap = 'Blues_r');
-plt.ylabel('Actual label');
-plt.xlabel('Predicted label');
+plt.ylabel('Actual label')
+plt.xlabel('Predicted label')
 all_sample_title = 'Overall Accuracy Score: {0}'.format(score)
-plt.title(all_sample_title, size = 15);
+plt.title(all_sample_title, size=15)
 plt.savefig('toy_Digits_ConfusionSeabornCodementor.png')
-plt.show();
+plt.show()
 
-###########################################
-#####  Display Misclassified Eights  ######
-###########################################
+#endregion
+
+#region  Display Misclassified Eights
 
 # as part of the assignment, we want to improve our classification accuracy for 8's
 # to get idea's for useful features, let's look at the 8's that were misclassified
@@ -104,10 +98,12 @@ for label, predict in zip(test_lbl, fullPrediction):
 
 print("There were " + str(len(misclassifiedIndexes)) + " misclassified 8's")
 
-plt.figure(figsize=(20,4))
+plt.figure(figsize=(20, 4))
 for plotIndex, badIndex in enumerate(misclassifiedIndexes[0:5]):
     plt.subplot(1, 5, plotIndex + 1)
     plt.imshow(np.reshape(test_img[badIndex], (28,28)), cmap=plt.cm.gray)
     plt.title('Predicted: {}, Actual: {}'.format(fullPrediction[badIndex],
-                                                 test_lbl[badIndex]), fontsize = 15)
+                                                 test_lbl[badIndex]), fontsize=15)
 plt.show()
+
+#endregion
